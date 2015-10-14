7d6dc52164edef60d09ba3cf7081b0843cb2036d
larva-pc <vliuzhihua@gmail.com>
2015-10-14 13:27:46 +0800

    init the project

---
+++ MineGuild/GameWorld.h
@@ -0,0 +1,12 @@
+#ifndef GAMEWORLD_H
+#define GAMEWORLD_H
+#include<string>
+using namespace std;
+class GameWorld{
+public:
+	bool InitWorld(string saveFilePath);//use to load save file, and will create the world which all living in
+
+
+};
+
+#endif
